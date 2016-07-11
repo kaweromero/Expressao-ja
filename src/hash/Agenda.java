cb0d155800eca3582eeb2c98d52bc3d6f7d62ea9
Kawê Romero <kaweromero@gmail.com>
2016-07-11 10:04:06 -0300

    HUE 4 HASH 2
    Fechado

---
+++ src/hash/Agenda.java
@@ -0,0 +1,36 @@
+package hash;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.swing.JOptionPane;
+
+public class Agenda {
+
+	private ArrayList<LinkedList<Contato>> agenda = new ArrayList<LinkedList<Contato>>();
+
+	public Agenda() {
+
+		for (int i = 0; i < 26; i++) {
+			LinkedList<Contato> lista = new LinkedList<Contato>();
+			agenda.add(lista);
+		}
+	}
+
+	public void inserirContato(Contato contato) {
+		try {
+			int indice = this.calculaHash(contato);
+			List<Contato> lista = this.agenda.get(indice);
+			lista.add(contato);
+
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, "Erro ao inserir contato, inicie o nome com letra maiuscula");
+		}
+	}
+
+	private int calculaHash(Contato contato) {
+		return contato.nome.toLowerCase().charAt(0) % 26;
+	}
+
+}
---
+++ src/hash/Contato.java
@@ -0,0 +1,29 @@
+package hash;
+
+public class Contato {
+
+	int telefones[];
+	String email[];
+	String nome;
+	
+	public int[] getTelefones() {
+		return telefones;
+	}
+	public void setTelefones(int[] telefones) {
+		this.telefones = telefones;
+	}
+	public String[] getEmail() {
+		return email;
+	}
+	public void setEmail(String[] email) {
+		this.email = email;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+}
